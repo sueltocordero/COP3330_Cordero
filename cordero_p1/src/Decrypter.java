46c2eed2ba30e78a099f8474bf6fc992c5bc609e
Luis Cordero <luisfcordero@yahoo.com>
2020-09-13 23:01:13 -0400

    Added Decrypter class

---
+++ cordero_p1/src/Decrypter.java
@@ -0,0 +1,18 @@
+public class Decrypter {
+    int position1;
+    int position2;
+    int position3;
+    int position4;
+    public String decrypt(String args){
+
+
+
+
+
+
+
+
+        String output = new String();
+        return output;
+    }
+}
