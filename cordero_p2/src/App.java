1602dfbd109fda9ba4e118b2b685a4f27bc28080
Luis Cordero <luisfcordero@yahoo.com>
2020-09-28 00:32:47 -0400

    Added final tweaks to code

--- cordero_p2/src/App.java
+++ cordero_p2/src/App.java
@@ -5,15 +5,17 @@ public class App {
     public static boolean moreInput() {
         System.out.println("Would you like to calculate your BMI?\n(Type Y or N)");
         Scanner in = new Scanner(System.in);
-        String choice = in.nextLine();
         while (true){
+            String choice = in.nextLine();
             if(choice.charAt(0)=='y' ||choice.charAt(0)=='Y'){
+
                 return true;
             }if(choice.charAt(0)=='n' ||choice.charAt(0)=='N'){
+
                 return false;
-            }else{
+            }if(choice.charAt(0)!='y' || choice.charAt(0)!='Y' ||choice.charAt(0)!='n' ||choice.charAt(0)!='N'){
                 System.out.println("Invalid input, do it again");
-                in.nextLine();
+
             }
 
         }
@@ -21,13 +23,15 @@ public class App {
     public static double getUserHeight() {
         Scanner in = new Scanner(System.in);
         System.out.println("Please provide height in inches");
-        int height = in.nextInt();
+        double height = in.nextDouble();
+        height = Math.abs(height);
         return height;
     }
     public static double getUserWeight() {
         Scanner in = new Scanner(System.in);
         System.out.println("Please provide weight in pounds");
-        int weight = in.nextInt();
+        double weight = in.nextDouble();
+        weight = Math.abs(weight);
         return weight;
     }
 
@@ -39,7 +43,10 @@ public class App {
         double total = 0;
         double loops = 0;
         for( int i =0 ; i < bmiData.size() ; i++){
-
+        BodyMassIndex tmp = (BodyMassIndex) bmiData.get(i);
+        double temp = tmp.returnBMI();
+        total= total + temp;
+        loops++;
         }
         double avgBMI = total/loops;
         System.out.println("The average BMI is " + avgBMI);
--- cordero_p2/src/BodyMassIndexTest.java
+++ cordero_p2/src/BodyMassIndexTest.java
@@ -1,3 +1,56 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class BodyMassIndexTest {
 
+    @Test
+    void underweightBMI(){
+        BodyMassIndex test = new BodyMassIndex(70,101.2);
+        //703*(101.2)/(60^2)=14.51 ~~ 14.5
+        assertEquals(14.5, test.returnBMI());
+    }
+    @Test
+    void normalBMI(){
+        BodyMassIndex test = new BodyMassIndex(80,205.6);
+        //703*(205.6)/(80^2)=22.58~~22.6
+        assertEquals(22.6, test.returnBMI());
+    }
+    @Test
+    void overweightBMI(){
+        BodyMassIndex test = new BodyMassIndex(71.5,200);
+        //703*(200)/(71.5^2)=27.50~~27.5
+        assertEquals(27.5, test.returnBMI());
+    }
+    @Test
+    void obeseBMI(){
+        BodyMassIndex test = new BodyMassIndex(74,300);
+        //703*(300)/(74^2)=38.51 ~~ 38.5
+        assertEquals(38.5, test.returnBMI());
+    }
+
+
+
+    @Test
+    void underweightCategory(){
+        BodyMassIndex test = new BodyMassIndex(70,101.2);
+        assertSame("Underweight",test.returnCategory());
+    }
+
+    @Test
+    void normalCategory(){
+        BodyMassIndex test = new BodyMassIndex(80,205.6);
+        assertSame("Normal",test.returnCategory());
+    }
+
+    @Test
+    void overweightCategory(){
+        BodyMassIndex test = new BodyMassIndex(71.5,200);
+        assertSame("Overweight",test.returnCategory());
+    }
+
+    @Test
+    void obeseCategory(){
+        BodyMassIndex test = new BodyMassIndex(74,300);
+        assertSame("Obese",test.returnCategory());
+    }
 }
