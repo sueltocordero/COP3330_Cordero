b15189987b23b00087ee18ac065fb61f457b8c14
Luis Cordero <luisfcordero@yahoo.com>
2020-09-27 22:48:25 -0400

    Added code to the BodyMassIndex class and App class

--- cordero_p2/src/App.java
+++ cordero_p2/src/App.java
@@ -1,2 +1,67 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class App {
+    public static boolean moreInput() {
+        System.out.println("Would you like to calculate your BMI?\n(Type Y or N)");
+        Scanner in = new Scanner(System.in);
+        String choice = in.nextLine();
+        while (true){
+            if(choice.charAt(0)=='y' ||choice.charAt(0)=='Y'){
+                return true;
+            }if(choice.charAt(0)=='n' ||choice.charAt(0)=='N'){
+                return false;
+            }else{
+                System.out.println("Invalid input, do it again");
+                in.nextLine();
+            }
+
+        }
+    }
+    public static double getUserHeight() {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Please provide height in inches");
+        int height = in.nextInt();
+        return height;
+    }
+    public static double getUserWeight() {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Please provide weight in pounds");
+        int weight = in.nextInt();
+        return weight;
+    }
+
+    public static void displayBmiInfo(BodyMassIndex bmi){
+        System.out.println("The BMI is " + bmi.returnBMI() + " and falls in "+ bmi.returnCategory() + " category");
+    }
+
+    public static void displayBmiStatistics(ArrayList bmiData){
+        double total = 0;
+        double loops = 0;
+        for( int i =0 ; i < bmiData.size() ; i++){
+
+        }
+        double avgBMI = total/loops;
+        System.out.println("The average BMI is " + avgBMI);
+    }
+
+
+    public static void main(String[] args) {
+        ArrayList<BodyMassIndex> bmiData = new ArrayList<BodyMassIndex>();
+
+        while (moreInput()) {
+            double height = getUserHeight();
+            double weight = getUserWeight();
+
+            BodyMassIndex bmi = new BodyMassIndex(height, weight);
+            bmiData.add(bmi);
+
+            displayBmiInfo(bmi);
+        }
+
+        displayBmiStatistics(bmiData);
+
+        }
+
 }
+
--- cordero_p2/src/BodyMassIndex.java
+++ cordero_p2/src/BodyMassIndex.java
@@ -1,2 +1,41 @@
+import static jdk.nashorn.internal.objects.NativeMath.round;
+
 public class BodyMassIndex {
+    private double BMI;
+    private String BMICategory;
+    public BodyMassIndex(double height, double weight) {
+        this.BMI = calcBMI(height, weight);
+
+        this.BMICategory = bmiCategory(this.BMI);
+    }
+
+    public static double calcBMI(double height, double weight){
+        double BMI = 703 * weight / (height * height);
+        BMI = BMI*10;
+        BMI = Math.round(BMI);
+        BMI = BMI/10;
+        return BMI;
+    }
+
+    public static String bmiCategory(double BMI){
+        if(BMI<18.5){
+            String under = "Underweight";
+            return under;
+        }if(BMI>18.5 && BMI<24.9){
+            String norm = "Normal";
+            return norm;
+        }if(BMI>25 && BMI<29.9){
+            String over = "Overweight";
+            return over;
+        }else{
+            String obese = "Obese";
+            return obese;
+        }
+    }
+    public double returnBMI(){
+        return this.BMI;
+    }
+    public String returnCategory(){
+        return this.BMICategory;
+    }
 }
